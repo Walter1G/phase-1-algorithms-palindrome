10f2129c79d3ca280829e5f5decd25df2d93fc84
Walter N <2760walter@gmail.com>
2023-10-07 08:55:03 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
 function isPalindrome(word) {
   // Write your algorithm here
+   const ispal=[]
+   const splitword=word.split('');
+   
+  for(let i=splitword.length-1; i>=0; i--){
+    ispal.push(splitword[i]);
+    
+  }
+
+
+  // console.log(`is pal: ${ispal}`)
+  // console.log(`is split: ${splitword}`)
+
+
+
+  // console.log(splitword);
+
+   console.log( `${word} -->${ispal.join('')}`);
+   if(word===ispal.join('')){
+    return true;
+   }else{
+    return false;
+   }
+
+
 }
+
+isPalindrome('robot')
 
 /* 
   Add your pseudocode here
 */
+//lower the case of the input
+//one approach is to create a new empty array and add letters in reverse 
+// if the array is same as the word then is palindrome
 
 /*
   Add written explanation of your solution here
